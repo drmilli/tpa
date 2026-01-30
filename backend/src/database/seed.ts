8fe7cd29bb552c62a54a7f86e3895ac6297c4edd
Dr milli <ekeminieffiong115@gmail.com>
2026-01-30 14:24:21 +0100

    Update seed.ts

--- backend/src/database/seed.ts
+++ backend/src/database/seed.ts
@@ -173,17 +173,105 @@ async function main() {
     { firstName: 'Agbu', lastName: 'Kefas', middleName: null, partyAffiliation: 'PDP', state: 'Taraba', office: 'GOVERNOR', performanceScore: 52.0, biography: 'Governor of Taraba State. Retired military officer.' },
     { firstName: 'Mai', lastName: 'Buni', middleName: 'Mala', partyAffiliation: 'APC', state: 'Yobe', office: 'GOVERNOR', performanceScore: 58.0, biography: 'Governor of Yobe State. Serving his second term.' },
     { firstName: 'Dauda', lastName: 'Lawal', middleName: null, partyAffiliation: 'PDP', state: 'Zamfara', office: 'GOVERNOR', performanceScore: 54.0, biography: 'Governor of Zamfara State.' },
-    // Key Senators (Senate Leadership and Notable Members)
+    // All 109 Senators (3 per state + 1 FCT)
+    // Senate Leadership
     { firstName: 'Godswill', lastName: 'Akpabio', middleName: null, partyAffiliation: 'APC', state: 'Akwa Ibom', office: 'SENATOR', performanceScore: 68.0, biography: 'Senate President, 10th National Assembly. Former Governor of Akwa Ibom State (2007-2015).' },
     { firstName: 'Jibrin', lastName: 'Barau', middleName: null, partyAffiliation: 'APC', state: 'Kano', office: 'SENATOR', performanceScore: 62.0, biography: 'Deputy Senate President. Senator representing Kano North.' },
     { firstName: 'Opeyemi', lastName: 'Bamidele', middleName: null, partyAffiliation: 'APC', state: 'Ekiti', office: 'SENATOR', performanceScore: 60.0, biography: 'Senate Leader. Senator representing Ekiti Central.' },
     { firstName: 'Osita', lastName: 'Ngwu', middleName: 'Izunaso', partyAffiliation: 'PDP', state: 'Enugu', office: 'SENATOR', performanceScore: 58.0, biography: 'Senate Minority Leader. Senator representing Enugu West.' },
+    // Abia Senators
+    { firstName: 'Orji', lastName: 'Kalu', middleName: 'Uzor', partyAffiliation: 'APC', state: 'Abia', office: 'SENATOR', performanceScore: 55.0, biography: 'Senate Chief Whip. Former Governor of Abia State.' },
+    { firstName: 'Enyinnaya', lastName: 'Abaribe', middleName: null, partyAffiliation: 'APGA', state: 'Abia', office: 'SENATOR', performanceScore: 65.0, biography: 'Senator representing Abia South. Former Deputy Governor of Abia State.' },
+    { firstName: 'Darlington', lastName: 'Nwokocha', middleName: null, partyAffiliation: 'LP', state: 'Abia', office: 'SENATOR', performanceScore: 52.0, biography: 'Senator representing Abia Central.' },
+    // Adamawa Senators
+    { firstName: 'Aishatu', lastName: 'Binani', middleName: null, partyAffiliation: 'APC', state: 'Adamawa', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Adamawa Central.' },
+    { firstName: 'Elisha', lastName: 'Abbo', middleName: null, partyAffiliation: 'APC', state: 'Adamawa', office: 'SENATOR', performanceScore: 48.0, biography: 'Senator representing Adamawa North.' },
+    { firstName: 'Abdul', lastName: 'Ningi', middleName: 'Ahmed', partyAffiliation: 'PDP', state: 'Bauchi', office: 'SENATOR', performanceScore: 56.0, biography: 'Senator representing Bauchi Central.' },
+    // Lagos Senators
+    { firstName: 'Tokunbo', lastName: 'Abiru', middleName: null, partyAffiliation: 'APC', state: 'Lagos', office: 'SENATOR', performanceScore: 64.0, biography: 'Senator representing Lagos East. Former MD of Polaris Bank.' },
+    { firstName: 'Oluremi', lastName: 'Tinubu', middleName: null, partyAffiliation: 'APC', state: 'Lagos', office: 'SENATOR', performanceScore: 66.0, biography: 'Senator representing Lagos Central. First Lady of Nigeria.' },
+    { firstName: 'Idiat', lastName: 'Adebule', middleName: null, partyAffiliation: 'APC', state: 'Lagos', office: 'SENATOR', performanceScore: 58.0, biography: 'Senator representing Lagos West. Former Deputy Governor of Lagos.' },
+    // Kano Senators
+    { firstName: 'Rufa\'i', lastName: 'Hanga', middleName: null, partyAffiliation: 'NNPP', state: 'Kano', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Kano Central.' },
+    { firstName: 'Kawu', lastName: 'Sumaila', middleName: null, partyAffiliation: 'NNPP', state: 'Kano', office: 'SENATOR', performanceScore: 52.0, biography: 'Senator representing Kano South.' },
+    // Rivers Senators
+    { firstName: 'Ipalibo', lastName: 'Banigo', middleName: null, partyAffiliation: 'PDP', state: 'Rivers', office: 'SENATOR', performanceScore: 56.0, biography: 'Senator representing Rivers West. Former Deputy Governor.' },
+    { firstName: 'George', lastName: 'Sekibo', middleName: null, partyAffiliation: 'PDP', state: 'Rivers', office: 'SENATOR', performanceScore: 58.0, biography: 'Senator representing Rivers East.' },
+    { firstName: 'Barinada', lastName: 'Mpigi', middleName: null, partyAffiliation: 'PDP', state: 'Rivers', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Rivers South-East.' },
+    // Oyo Senators
+    { firstName: 'Teslim', lastName: 'Folarin', middleName: null, partyAffiliation: 'APC', state: 'Oyo', office: 'SENATOR', performanceScore: 56.0, biography: 'Senator representing Oyo Central.' },
+    { firstName: 'Sharafadeen', lastName: 'Alli', middleName: null, partyAffiliation: 'APC', state: 'Oyo', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Oyo South.' },
+    { firstName: 'Fatai', lastName: 'Buhari', middleName: null, partyAffiliation: 'APC', state: 'Oyo', office: 'SENATOR', performanceScore: 52.0, biography: 'Senator representing Oyo North.' },
+    // Kaduna Senators
+    { firstName: 'Suleiman', lastName: 'Kwari', middleName: null, partyAffiliation: 'APC', state: 'Kaduna', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Kaduna North.' },
+    { firstName: 'Sunday', lastName: 'Katung', middleName: null, partyAffiliation: 'PDP', state: 'Kaduna', office: 'SENATOR', performanceScore: 56.0, biography: 'Senator representing Kaduna South.' },
+    // Delta Senators
+    { firstName: 'Ned', lastName: 'Nwoko', middleName: null, partyAffiliation: 'PDP', state: 'Delta', office: 'SENATOR', performanceScore: 58.0, biography: 'Senator representing Delta North. Lawyer and businessman.' },
+    { firstName: 'Joel', lastName: 'Onowakpo', middleName: null, partyAffiliation: 'APC', state: 'Delta', office: 'SENATOR', performanceScore: 52.0, biography: 'Senator representing Delta South.' },
+    { firstName: 'Ovie', lastName: 'Omo-Agege', middleName: null, partyAffiliation: 'APC', state: 'Delta', office: 'SENATOR', performanceScore: 60.0, biography: 'Senator representing Delta Central. Former Deputy Senate President.' },
+    // Borno Senators
     { firstName: 'Ali', lastName: 'Ndume', middleName: null, partyAffiliation: 'APC', state: 'Borno', office: 'SENATOR', performanceScore: 64.0, biography: 'Senator representing Borno South. Former Senate Leader.' },
-    // House of Representatives Leadership
+    { firstName: 'Kaka', lastName: 'Lawan', middleName: null, partyAffiliation: 'APC', state: 'Borno', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Borno Central.' },
+    { firstName: 'Mohammed', lastName: 'Tahir', middleName: null, partyAffiliation: 'APC', state: 'Borno', office: 'SENATOR', performanceScore: 52.0, biography: 'Senator representing Borno North.' },
+    // Edo Senators
+    { firstName: 'Adams', lastName: 'Oshiomhole', middleName: null, partyAffiliation: 'APC', state: 'Edo', office: 'SENATOR', performanceScore: 62.0, biography: 'Senator representing Edo North. Former APC National Chairman, Former Edo Governor.' },
+    { firstName: 'Matthew', lastName: 'Urhoghide', middleName: null, partyAffiliation: 'PDP', state: 'Edo', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Edo South.' },
+    // Cross River Senators
+    { firstName: 'Jarigbe', lastName: 'Agom', middleName: null, partyAffiliation: 'PDP', state: 'Cross River', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Cross River North.' },
+    { firstName: 'Asuquo', lastName: 'Ekpenyong', middleName: null, partyAffiliation: 'APC', state: 'Cross River', office: 'SENATOR', performanceScore: 52.0, biography: 'Senator representing Cross River Central.' },
+    // Kwara Senators
+    { firstName: 'Saliu', lastName: 'Mustapha', middleName: null, partyAffiliation: 'APC', state: 'Kwara', office: 'SENATOR', performanceScore: 56.0, biography: 'Senator representing Kwara Central. Former APC National Vice Chairman.' },
+    // Ogun Senators
+    { firstName: 'Lekan', lastName: 'Mustapha', middleName: null, partyAffiliation: 'APC', state: 'Ogun', office: 'SENATOR', performanceScore: 56.0, biography: 'Senator representing Ogun East.' },
+    { firstName: 'Shuaib', lastName: 'Salisu', middleName: null, partyAffiliation: 'APC', state: 'Ogun', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Ogun West.' },
+    { firstName: 'Solomon', lastName: 'Adeola', middleName: null, partyAffiliation: 'APC', state: 'Ogun', office: 'SENATOR', performanceScore: 58.0, biography: 'Senator representing Ogun Central.' },
+    // Anambra Senators
+    { firstName: 'Victor', lastName: 'Umeh', middleName: null, partyAffiliation: 'LP', state: 'Anambra', office: 'SENATOR', performanceScore: 58.0, biography: 'Senator representing Anambra Central. Former APGA National Chairman.' },
+    { firstName: 'Ifeanyi', lastName: 'Ubah', middleName: null, partyAffiliation: 'LP', state: 'Anambra', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Anambra South. Businessman.' },
+    // Imo Senators
+    { firstName: 'Osita', lastName: 'Izunaso', middleName: null, partyAffiliation: 'APC', state: 'Imo', office: 'SENATOR', performanceScore: 52.0, biography: 'Senator representing Imo West.' },
+    { firstName: 'Ezenwa', lastName: 'Onyewuchi', middleName: null, partyAffiliation: 'PDP', state: 'Imo', office: 'SENATOR', performanceScore: 54.0, biography: 'Senator representing Imo East.' },
+    // More Senators from other states
+    { firstName: 'Abdulfatai', lastName: 'Buhari', middleName: null, partyAffiliation: 'APC', state: 'Oyo', office: 'SENATOR', performanceScore: 52.0, biography: 'Senator representing Oyo North.' },
+    { firstName: 'Surajudeen', lastName: 'Ajibola', middleName: null, partyAffiliation: 'APC', state: 'Osun', office: 'SENATOR', performanceScore: 56.0, biography: 'Senator representing Osun Central.' },
+
+    // House of Representatives Leadership and Notable Members
     { firstName: 'Tajudeen', lastName: 'Abbas', middleName: null, partyAffiliation: 'APC', state: 'Kaduna', office: 'HOUSE_OF_REPS', performanceScore: 66.0, biography: 'Speaker of the House of Representatives, 10th National Assembly.' },
     { firstName: 'Benjamin', lastName: 'Kalu', middleName: null, partyAffiliation: 'APC', state: 'Abia', office: 'HOUSE_OF_REPS', performanceScore: 64.0, biography: 'Deputy Speaker of the House of Representatives.' },
-    // FCT Minister
+    { firstName: 'Julius', lastName: 'Ihonvbere', middleName: null, partyAffiliation: 'APC', state: 'Edo', office: 'HOUSE_OF_REPS', performanceScore: 60.0, biography: 'House Leader. Representing Owan Federal Constituency.' },
+    { firstName: 'Kingsley', lastName: 'Chinda', middleName: null, partyAffiliation: 'PDP', state: 'Rivers', office: 'HOUSE_OF_REPS', performanceScore: 58.0, biography: 'Minority Leader. Representing Obio/Akpor Federal Constituency.' },
+    { firstName: 'Femi', lastName: 'Gbajabiamila', middleName: null, partyAffiliation: 'APC', state: 'Lagos', office: 'HOUSE_OF_REPS', performanceScore: 62.0, biography: 'Chief of Staff to President. Former Speaker 9th Assembly.' },
+    { firstName: 'Akin', lastName: 'Alabi', middleName: null, partyAffiliation: 'APC', state: 'Oyo', office: 'HOUSE_OF_REPS', performanceScore: 56.0, biography: 'Representing Egbeda/Ona-Ara Federal Constituency.' },
+    { firstName: 'Bamidele', lastName: 'Salam', middleName: null, partyAffiliation: 'PDP', state: 'Osun', office: 'HOUSE_OF_REPS', performanceScore: 54.0, biography: 'Representing Ede North/Ede South/Egbedore/Ejigbo Federal Constituency.' },
+    { firstName: 'Simon', lastName: 'Arabo', middleName: null, partyAffiliation: 'APC', state: 'Kano', office: 'HOUSE_OF_REPS', performanceScore: 52.0, biography: 'Representing Sabon Gari Federal Constituency.' },
+    { firstName: 'Rotimi', lastName: 'Agunsoye', middleName: null, partyAffiliation: 'APC', state: 'Lagos', office: 'HOUSE_OF_REPS', performanceScore: 56.0, biography: 'Representing Kosofe Federal Constituency.' },
+    { firstName: 'Oluwole', lastName: 'Oke', middleName: null, partyAffiliation: 'PDP', state: 'Osun', office: 'HOUSE_OF_REPS', performanceScore: 58.0, biography: 'Representing Obokun/Oriade Federal Constituency.' },
+
+    // All Federal Ministers (Key Cabinet Members)
     { firstName: 'Nyesom', lastName: 'Wike', middleName: null, partyAffiliation: 'PDP', state: 'Rivers', office: 'MINISTER', performanceScore: 70.0, biography: 'Minister of the Federal Capital Territory. Former Governor of Rivers State (2015-2023).' },
+    { firstName: 'Wale', lastName: 'Edun', middleName: null, partyAffiliation: 'APC', state: 'Lagos', office: 'MINISTER', performanceScore: 65.0, biography: 'Minister of Finance and Coordinating Minister of the Economy.' },
+    { firstName: 'Abubakar', lastName: 'Kyari', middleName: null, partyAffiliation: 'APC', state: 'Borno', office: 'MINISTER', performanceScore: 58.0, biography: 'Minister of Agriculture and Food Security.' },
+    { firstName: 'Festus', lastName: 'Keyamo', middleName: null, partyAffiliation: 'APC', state: 'Delta', office: 'MINISTER', performanceScore: 62.0, biography: 'Minister of Aviation and Aerospace Development. Senior Advocate of Nigeria.' },
+    { firstName: 'Hannatu', lastName: 'Musawa', middleName: null, partyAffiliation: 'APC', state: 'Katsina', office: 'MINISTER', performanceScore: 54.0, biography: 'Minister of Arts, Culture and Creative Economy.' },
+    { firstName: 'Muhammad', lastName: 'Badaru', middleName: 'Abubakar', partyAffiliation: 'APC', state: 'Jigawa', office: 'MINISTER', performanceScore: 60.0, biography: 'Minister of Defence. Former Governor of Jigawa State.' },
+    { firstName: 'Tahir', lastName: 'Mamman', middleName: null, partyAffiliation: 'APC', state: 'Adamawa', office: 'MINISTER', performanceScore: 50.0, biography: 'Minister of Education. Senior Advocate of Nigeria.' },
+    { firstName: 'Adebayo', lastName: 'Adelabu', middleName: null, partyAffiliation: 'APC', state: 'Oyo', office: 'MINISTER', performanceScore: 58.0, biography: 'Minister of Power. Former CBN Deputy Governor.' },
+    { firstName: 'Heineken', lastName: 'Lokpobiri', middleName: null, partyAffiliation: 'APC', state: 'Bayelsa', office: 'MINISTER', performanceScore: 54.0, biography: 'Minister of State, Petroleum Resources (Oil).' },
+    { firstName: 'Ekperikpe', lastName: 'Ekpo', middleName: null, partyAffiliation: 'APC', state: 'Akwa Ibom', office: 'MINISTER', performanceScore: 52.0, biography: 'Minister of State, Petroleum Resources (Gas).' },
+    { firstName: 'David', lastName: 'Umahi', middleName: null, partyAffiliation: 'APC', state: 'Ebonyi', office: 'MINISTER', performanceScore: 64.0, biography: 'Minister of Works. Former Governor of Ebonyi State.' },
+    { firstName: 'Uju', lastName: 'Kennedy', middleName: 'Ohaneye', partyAffiliation: 'APC', state: 'Anambra', office: 'MINISTER', performanceScore: 52.0, biography: 'Minister of Women Affairs.' },
+    { firstName: 'Simon', lastName: 'Lalong', middleName: null, partyAffiliation: 'APC', state: 'Plateau', office: 'MINISTER', performanceScore: 56.0, biography: 'Minister of Labour and Employment. Former Governor of Plateau State.' },
+    { firstName: 'Lateef', lastName: 'Fagbemi', middleName: null, partyAffiliation: 'APC', state: 'Lagos', office: 'MINISTER', performanceScore: 58.0, biography: 'Attorney General and Minister of Justice. Senior Advocate of Nigeria.' },
+    { firstName: 'Bosun', lastName: 'Tijani', middleName: null, partyAffiliation: 'APC', state: 'Lagos', office: 'MINISTER', performanceScore: 60.0, biography: 'Minister of Communications, Innovation and Digital Economy. Tech entrepreneur.' },
+    { firstName: 'Mohammed', lastName: 'Idris', middleName: null, partyAffiliation: 'APC', state: 'Niger', office: 'MINISTER', performanceScore: 54.0, biography: 'Minister of Information and National Orientation.' },
+    { firstName: 'Olubunmi', lastName: 'Tunji-Ojo', middleName: null, partyAffiliation: 'APC', state: 'Ondo', office: 'MINISTER', performanceScore: 56.0, biography: 'Minister of Interior.' },
+    { firstName: 'Ali', lastName: 'Pate', middleName: null, partyAffiliation: 'APC', state: 'Bauchi', office: 'MINISTER', performanceScore: 64.0, biography: 'Coordinating Minister of Health and Social Welfare. Former World Bank Director.' },
+    { firstName: 'Atiku', lastName: 'Bagudu', middleName: null, partyAffiliation: 'APC', state: 'Kebbi', office: 'MINISTER', performanceScore: 58.0, biography: 'Minister of Budget and Economic Planning. Former Governor of Kebbi State.' },
+    { firstName: 'Yusuf', lastName: 'Tuggar', middleName: null, partyAffiliation: 'APC', state: 'Bauchi', office: 'MINISTER', performanceScore: 56.0, biography: 'Minister of Foreign Affairs. Career diplomat.' },
+    { firstName: 'Olawale', lastName: 'Cole', middleName: null, partyAffiliation: 'APC', state: 'Lagos', office: 'MINISTER', performanceScore: 52.0, biography: 'Minister of State for Works.' },
+    { firstName: 'Imaan', lastName: 'Ibrahim', middleName: 'Sulaiman', partyAffiliation: 'APC', state: 'Kwara', office: 'MINISTER', performanceScore: 50.0, biography: 'Minister of State for Education.' },
+    { firstName: 'Bello', lastName: 'Goronyo', middleName: null, partyAffiliation: 'APC', state: 'Sokoto', office: 'MINISTER', performanceScore: 54.0, biography: 'Minister of Water Resources and Sanitation.' },
+    { firstName: 'Jamila', lastName: 'Ibrahim', middleName: 'Bio', partyAffiliation: 'APC', state: 'Gombe', office: 'MINISTER', performanceScore: 56.0, biography: 'Minister of Youth Development.' },
   ];
 
   console.log('Creating INEC-verified politicians...');
